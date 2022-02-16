0d6be26bb2fd53a2cf1f5b0a212b14937e764d50
Leonid Sergeev <sls@bfg.ai>
2022-02-16 18:58:24 +0400

    тестовая реализация отображения кнопки в виде шаблона

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log('allo');
+import compileTemplate from './components/button/button.pug';
+
+const app = document.getElementById('app');
+app.innerHTML = compileTemplate({ style: 'error', title: 'awesome', type: 'submit' });
