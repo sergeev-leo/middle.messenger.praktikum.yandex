6c909e49eea3f4a30a721da188478b4c4fd20df5
Leonid Sergeev <sls@bfg.ai>
2022-02-17 21:16:44 +0400

    - корректировки компонента сообщения

---
+++ src/components/message/message.ts
@@ -0,0 +1,20 @@
+import { Block } from "../../modules/Block/Block";
+import compileTemplate from './message.pug';
+
+
+type TMessageProps = {
+  currentUserMessage: boolean,
+  status: string,
+  text: string,
+  time: string,
+};
+
+export class Message extends Block {
+  constructor(props: TMessageProps) {
+    super("div", props);
+  }
+
+  render() {
+    return this.compile(compileTemplate, this.props);
+  }
+}
