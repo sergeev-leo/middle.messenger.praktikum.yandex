dafb3f7fffffa0e9c4d1e9401950c173bf52afe3
Leonid Sergeev <sls@bfg.ai>
2022-02-17 20:43:00 +0400

    - корректировки компонента кнопки с иконкой

---
+++ src/components/icon-button/icon-button.ts
@@ -0,0 +1,19 @@
+import { Block } from "../../modules/Block/Block";
+import compileTemplate from './icon-button.pug';
+
+
+type TIconButtonProps = {
+  iconClassName: string,
+  style?: string,
+  outerIconClassName?: string,
+};
+
+export class IconButton extends Block {
+  constructor(props: TIconButtonProps) {
+    super("div", props);
+  }
+
+  render() {
+    return this.compile(compileTemplate, this.props);
+  }
+}
