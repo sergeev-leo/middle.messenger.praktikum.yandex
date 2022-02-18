6f0e80998b89181d5a2b0e787f4b54ea46716556
Leonid Sergeev <sls@bfg.ai>
2022-02-18 10:53:44 +0400

    - переработан компонент меню - проработан компонент элемента меню

--- src/components/menuItem/menuItem.ts
+++ src/components/menuItem/menuItem.ts
@@ -1,44 +1,38 @@
 import { Block } from '../../modules/Block/Block';
-import compileTemplate from './menu.pug';
+import compileTemplate from '../menuItem/menuItem.pug';
 import { IconButton, TIconButtonProps } from '../icon-button/icon-button';
 import { TEvents } from '../../modules/types';
-import { MenuItem, TMenuItemProps } from '../menuItem/menuItem';
 
 
-
-export type TMenuProps = {
+export type TMenuItemProps = {
+  id: string,
+  text: string,
   iconButton: TIconButtonProps,
-  position?: string,
-  data: TMenuItemProps[],
   events?: TEvents,
-};
+}
 
-export class Menu extends Block {
-  constructor(props: TMenuProps) {
+export class MenuItem extends Block {
+  constructor(props: TMenuItemProps) {
     super('div', props);
-
   }
 
   render() {
     const {
-      data,
+      text,
+      id,
       iconButton,
-    } = this.props as TMenuProps;
+    } = this.props as TMenuItemProps;
 
     //------------------------------------------------------------------
     this._children.iconButton = new IconButton(iconButton);
-    data.forEach(({ id, text, iconButton, events }, index) => {
-      const key = ['menuItem', index].join('_');
-
-      this._children[key] = new MenuItem({ id, text, iconButton, events });
-    });
     //------------------------------------------------------------------
 
     return this.compile(
       compileTemplate,
       {
-        ...this._children,
-        data,
+        id,
+        text,
+        iconButton: this._children.iconButton,
       },
     );
   }
