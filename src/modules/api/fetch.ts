b44b422505d7bb520f2b58d7438db6953f37ef38
Leonid Sergeev <sls@bfg.ai>
2022-02-22 17:17:34 +0400

    - корректировка логики fetchAPI

--- src/modules/api/fetch.ts
+++ src/modules/api/fetch.ts
@@ -2,65 +2,60 @@ enum HTTP_METHOD {
   GET = 'GET',
   POST = 'POST',
   PUT = 'PUT',
-  PATCH = 'PATCH',
   DELETE = 'DELETE',
 }
 
-type TOptions = {
+type THttpRequestOptions = {
   method: HTTP_METHOD,
   data?: Record<string, unknown>,
+  headers?: Record<string, string>,
+  timeout?: number,
 };
 
-type TOptionsWithoutMethod = Omit<TOptions, 'method'>;
-
-const makeHTTPRequest = (
-  url: string,
-  options: TOptions,
-): Promise<XMLHttpRequest> => {
-  const {
-    method,
-    data,
-  } = options;
-
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-
-    xhr.setRequestHeader('Content-Type', 'text/plain');
-
-    xhr.onload = () => {
-      resolve(xhr);
-    };
-
-    xhr.onabort = reject;
-    xhr.onerror = reject;
-    xhr.ontimeout = reject;
-
-    if (method === 'GET' || !data) {
-      xhr.send();
-    } else {
-      xhr.send(JSON.stringify(data));
-    }
-  });
-};
+type THttpRequestOptionsWithoutMethod = Omit<THttpRequestOptions, 'method'>;
 
 class HTTPTransport {
-  get(url: string, options: TOptionsWithoutMethod = {}): Promise<XMLHttpRequest> {
-    return this.makeHTTPRequest(url, { ...options, method: HTTP_METHOD.GET });
+  get(url: string, options: THttpRequestOptionsWithoutMethod = {}): Promise<XMLHttpRequest> {
+    return this.makeHTTPRequest(url, { ...options, method: HTTP_METHOD.GET }, options.timeout);
   }
 
-  makeHTTPRequest(
+  post(url: string, options: THttpRequestOptionsWithoutMethod = {}): Promise<XMLHttpRequest> {
+    return this.makeHTTPRequest(url, { ...options, method: HTTP_METHOD.POST }, options.timeout);
+  }
+
+  put(url: string, options: THttpRequestOptionsWithoutMethod = {}): Promise<XMLHttpRequest> {
+    return this.makeHTTPRequest(url, { ...options, method: HTTP_METHOD.PUT }, options.timeout);
+  }
+
+  delete(url: string, options: THttpRequestOptionsWithoutMethod = {}): Promise<XMLHttpRequest> {
+    return this.makeHTTPRequest(url, { ...options, method: HTTP_METHOD.DELETE }, options.timeout);
+  }
+
+  private makeHTTPRequest(
     url: string,
-    options: TOptions = { method: HTTP_METHOD.GET },
+    options: THttpRequestOptions = { method: HTTP_METHOD.GET },
+    timeout = 60000,
   ): Promise<XMLHttpRequest> {
     const {
       method,
       data,
+      headers,
     } = options;
 
     return new Promise((resolve, reject) => {
+      const urlToRequest = [url, this.queryStringify(data)].join('');
+
       const xhr = new XMLHttpRequest();
-      xhr.open(method, url);
+
+      // задаем указанные заголовки
+      for(const header in headers) {
+        xhr.setRequestHeader(header, headers[header]);
+      }
+
+      // инициализируем таймаут
+      xhr.timeout = timeout;
+
+      xhr.open(method, urlToRequest);
 
       xhr.onload = function() {
         resolve(xhr);
@@ -68,7 +63,9 @@ class HTTPTransport {
 
       xhr.onabort = reject;
       xhr.onerror = reject;
-      xhr.ontimeout = reject;
+      xhr.ontimeout = () => {
+        throw new Error('request timeout error');
+      };
 
       if (method === HTTP_METHOD.GET || !data) {
         xhr.send();
@@ -76,5 +73,26 @@ class HTTPTransport {
         xhr.send(JSON.stringify(data));
       }
     });
+  }
+
+  private queryStringify(data?: Record<string, unknown>): string {
+    if(!data || Object.keys(data).length === 0) {
+      return '';
+    }
+    // Можно делать трансформацию GET-параметров в отдельной функции
+    return Object
+      .keys(data)
+      .reduce(
+        (acc, param, index) => {
+          const value = data[param];
+          const paramBlock = [param, value].join('=');
+
+          acc = index === Object.keys(data).length -1 ?
+            acc + paramBlock :
+            acc + paramBlock + '&';
+          return acc;
+        },
+        '?',
+      );
   }
 }
