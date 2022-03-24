3163e48ef3e96ae54bab51eb460863168305c15b
Leonid Sergeev <sls@bfg.ai>
2022-03-24 19:11:58 +0400

    test

--- test/block.test.ts
+++ test/block.test.ts
@@ -7,7 +7,7 @@ describe('модуль Block', () => {
       events: {},
     });
 
-    const didMount = jest.spyOn(Component, 'getContent');
+    const didMount = jest.spOn(Component, 'getContent');
 
     Component.hide();
 
